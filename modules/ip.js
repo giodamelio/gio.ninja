06fc932e7fda4441a1996996b4dacc969556fb71
Gio d'Amelio <giodamelio@gmail.com>
2014-06-13 23:25:30 -0700

    Add ip json

--- modules/ip.js
+++ modules/ip.js
@@ -2,7 +2,15 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-    res.send(200, req.headers["x-forwarded-for"] || req.connection.remoteAddress);
+    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    res.send(200, ip);
+});
+
+app.get("/.json", function(req, res) {
+    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    res.json(200, {
+        ip: ip
+    });
 });
 
 module.exports = app;
