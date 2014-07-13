22740d2ee962ccf069527f0197c7cd04b8c77414
Gio d'Amelio <giodamelio@gmail.com>
2014-07-12 20:02:22 -0700

    npm: json support

--- modules/npm/index.js
+++ modules/npm/index.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 
 var express = require("express");
+var request = require("request");
 
 var utils = require("../../utils");
 
@@ -8,16 +9,20 @@ var router = module.exports = express.Router();
 
 // Npm
 var getNpm = function(req, res) {
-    res.format({
-        "text/plain": function() {
-            res.send("");
-        },
-        "text/html": function() {
-            res.send("");
-        },
-        "application/json": function() {
-            res.json({});
-        }
+    // Get npm info
+    var npmBaseUrl = "https://skimdb.npmjs.com/registry/";
+    request(npmBaseUrl + req.params.packageName, function(err, response, body) {
+        res.format({
+            "text/plain": function() {
+                res.send("");
+            },
+            "text/html": function() {
+                res.send("");
+            },
+            "application/json": function() {
+                res.json(JSON.parse(body));
+            }
+        });
     });
 };
 router.get("/:packageName", getNpm);
