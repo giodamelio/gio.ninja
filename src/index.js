69aaa9d8c3647d18c1ddbe81145d945833c5e6be
Gio d'Amelio <giodamelio@gmail.com>
2014-08-14 22:23:32 -0700

    Loop through and add modules dynamically

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var path = require("path");
+var fs = require("fs");
 
 var hapi = require("hapi");
+var bluebird = require("bluebird");
 
 var server = new hapi.Server("localhost", 3141);
 
@@ -13,12 +15,26 @@ server.route({
     }
 });
 
-server.pack.register([
-    require("./modules/timer")
-], function (error) {
-    if (error) console.log(error);
-    server.start(function() {
-        console.log("Server running at:", server.info.uri);
+// Serve the modules
+bluebird.all(fs.readdirSync(path.resolve(__dirname, "modules"))
+    .map(function(moduleName) {
+        return new bluebird(function(resolve, reject) {
+            var module = require(path.resolve(__dirname, "modules", moduleName));
+            server.pack.register(module, function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(module);
+                }
+            });
+        });
+    }))
+
+    // Start the server
+    .then(function(modules) {
+        server.start(function() {
+            console.log("Server running at:", server.info.uri);
+        });
     });
-});
+
 
