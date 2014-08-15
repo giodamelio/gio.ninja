4887ea05e558de0dfe0dd6d07fb2ba338bd3872d
Gio d'Amelio <giodamelio@gmail.com>
2014-08-14 21:26:13 -0700

    Cleanup old version

--- utils.js
+++
@@ -1,8 +0,0 @@
-// Force the accept header to a certin value
-module.exports.forceAccept = function(mimeType) {
-    return function(req, res, next) {
-        req.headers.accept = mimeType;
-        next();
-    };
-};
-
