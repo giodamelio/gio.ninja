1e65957b3c8bc2259db6490e08f42390abcaa276
Gio d'Amelio <giodamelio@gmail.com>
2014-08-14 21:31:43 -0700

    Serve favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-var util = require("util");
+var path = require("path");
 
 var hapi = require("hapi");
 
 var server = new hapi.Server("localhost", 3141);
+
+// Serve the favicon
+server.route({
+    method: "GET",
+    path: "/favicon.ico",
+    handler: {
+        file: path.resolve(__dirname, "../favicon.ico")
+    }
+});
 
 server.pack.register([
     require("./modules/timer")
